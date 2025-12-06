43566978dce0730feb7ea1dd4270b3cde63c1c21
subhodeep2005s <sarkarsubhodeep417@gmail.com>
2025-12-06 23:44:47 +0530

    refactor: comment out helmet and morgan middleware for potential performance improvement

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,8 +15,7 @@ import orderRoutes from "./routes/order.routes.js";
 import reviewRoutes from "./routes/review.routes.js";
 import productRoutes from "./routes/products.route.js";
 import cartRoutes from "./routes/cart.routes.js";
-import helmet from "helmet";
-import morgan from "morgan";
+
 import cors from "cors";
 const app = express();
 
@@ -25,8 +24,8 @@ app.use(express.json());
 app.use(clerkMiddleware());
 
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
-app.use(helmet());
-app.use(morgan("dev"));
+// app.use(helmet());
+// app.use(morgan("dev"));
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
